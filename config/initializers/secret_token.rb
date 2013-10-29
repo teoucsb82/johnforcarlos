03cb5bd431edacdc5580c074c0ee729285b65441
Teo Dell'Amico <teo.dellamico@gmail.com>
2013-10-29 15:24:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Johnforcarlos::Application.config.secret_key_base = 'ee2e3939ee14836bb16bdc34d07a3792bc48a7d4d97d8570cd256eb8788db2feae75338cd3b393901c38408367caf9fc081a557f9cbdd1b3ba7cebc0e109e780'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Johnforcarlos::Application.config.session_store :cookie_store, key: '_johnforcarlos_session'
