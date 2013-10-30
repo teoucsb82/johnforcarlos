9893ce4bb5a2313b9cca977c32cfadf77481e16f
Teo Dell'Amico <teo.dellamico@gmail.com>
2013-10-30 14:26:52 -0700

    added admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131030203722) do
+ActiveRecord::Schema.define(version: 20131030212636) do
 
   create_table "donations", force: true do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20131030203722) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
